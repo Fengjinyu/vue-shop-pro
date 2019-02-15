d815216fdbe8e916c641eda0782d4a8124ad3049
冯晋宇 <961173701@qq.com>
2019-02-15 16:49:21 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -4,12 +4,29 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入css样式全局控制
+import './assets/css/global.css'
+
+// 引入element-ui
+import ElementUI from 'element-ui'
+
+// 引入字体图标库
+import './assets/fonts/iconfont.css'
+
+// 引入axios
+import axios from 'axios'
+// 配置axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+// 注册标签组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/home'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  // 访问路径为/login 就直接pass
+  if (to.path === '/login') {
+    return next()
+  }
+  // 访问的路径不是/login 判断token
+  var token = window.sessionStorage.getItem('token')
+  if (!token) {
+    return next('/login')
+  }
+  next()
+})
+export default router
